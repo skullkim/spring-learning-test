feb73242e3d2f3eb62bedb2fba0a3ff9f20c7db7
skullKim <kyk990328@gmail.com>
2022-04-19 16:13:42 +0900

    handler 학습 테스트 완료

--- src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
+++ src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
@@ -1,6 +1,8 @@
 package nextstep.helloworld.mvc.handler;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import nextstep.helloworld.mvc.domain.User;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,19 +11,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/return-value")
 public class ReturnValueController {
 
-//    public void string() {
-//        return "message";
-//    }
-//
-//    public void responseBodyForUser() {
-//        return new User("name", "email");
-//    }
-//
-//    public void responseEntity(@PathVariable Long id) {
-//        return ResponseEntity.ok(new User("name", "email"));
-//    }
-//
-//    public void responseEntityFor400() {
-//        return ResponseEntity.badRequest().build();
-//    }
+    @GetMapping(path="/message", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String string() {
+        return "message";
+    }
+
+    @GetMapping(path="/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public User responseBodyForUser() {
+        return new User("name", "email");
+    }
+
+    @GetMapping(path="/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity responseEntity(@PathVariable Long id) {
+        return ResponseEntity.ok(new User("name", "email"));
+    }
+
+    @GetMapping(path="/members")
+    public ResponseEntity responseEntityFor400() {
+        return ResponseEntity.badRequest().build();
+    }
 }
