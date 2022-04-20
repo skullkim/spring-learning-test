735591488e37fd0078f41263aea3bbd1e0562dbd
skullKim <kyk990328@gmail.com>
2022-04-20 10:55:48 +0900

    exception 학습 테스트 완료

--- src/main/java/nextstep/helloworld/mvc/exceptions/ExceptionsController.java
+++ src/main/java/nextstep/helloworld/mvc/exceptions/ExceptionsController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +23,7 @@ public class ExceptionsController {
         throw new HelloException();
     }
 
+    @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> handle() {
         return ResponseEntity.badRequest().body("CustomException");
     }
